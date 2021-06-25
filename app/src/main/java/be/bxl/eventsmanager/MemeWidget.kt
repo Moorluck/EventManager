047a1c6107b426b06c794e264aae128186828a57
William <wgenard94@gmail.com>
2021-06-25 14:26:47 +0200

    fix widget

--- app/src/main/java/be/bxl/eventsmanager/MemeWidget.kt
+++ app/src/main/java/be/bxl/eventsmanager/MemeWidget.kt
@@ -1,21 +1,19 @@
 package be.bxl.eventsmanager
 
-import android.app.Dialog
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
-import android.widget.ImageView
+import android.util.Log
 import android.widget.RemoteViews
-import android.widget.TextView
 import be.bxl.eventsmanager.api.HttpRequest
 import be.bxl.eventsmanager.api.MemeParse
 import be.bxl.eventsmanager.api.URLHelper
 import be.bxl.eventsmanager.models.Meme
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.AppWidgetTarget
 import com.bumptech.glide.request.transition.Transition
 import kotlinx.coroutines.*
@@ -36,41 +34,28 @@ class MemeWidget : AppWidgetProvider() {
     ) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+
+            val views = RemoteViews(context.packageName, R.layout.meme_widget)
+
+            requestMeme(context, views, appWidgetId)
+
+            views.setOnClickPendingIntent(R.id.img_widget_meme, getPendingSelfIntent(context, ON_CLICK_MEME, appWidgetIds))
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
         }
     }
 
-    override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
-
-    private fun updateAppWidget(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetId: Int
-    ) {
-        // Construct the RemoteViews object
-        val views = RemoteViews(context.packageName, R.layout.meme_widget)
-        requestMeme(context, views, appWidgetId)
-
-        views.setOnClickPendingIntent(R.id.img_widget_meme, getPendingSelfIntent(context, ON_CLICK_MEME, appWidgetId, appWidgetManager))
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views)
-    }
-
-    private fun getPendingSelfIntent(context: Context, action : String, appWidgetId: Int, appWidgetManager : AppWidgetManager): PendingIntent? {
+    private fun getPendingSelfIntent(context: Context, action: String, appWidgetIds: IntArray): PendingIntent? {
         val intent = Intent(context, MemeWidget::class.java)
         intent.setAction(action)
-        intent.putExtra("APP_WIDGET_ID", appWidgetId)
+        intent.putExtra("appWidgetIds", appWidgetIds)
         return PendingIntent.getBroadcast(context, 0, intent, 0)
     }
 
     private fun requestMeme(context: Context, views: RemoteViews, appWidgetId: Int) {
-        CoroutineScope(Dispatchers.IO).launch {
+
+        val requestScope = CoroutineScope(Dispatchers.IO).launch {
             val url = URLHelper.URLMeme
             val request = HttpRequest.getJsonFromRequest(url)
             if (request != null) {
@@ -78,12 +63,16 @@ class MemeWidget : AppWidgetProvider() {
                 updateMemeUI(context, views, appWidgetId, meme)
             }
         }
+
     }
 
-
-    private suspend fun updateMemeUI(context: Context, views : RemoteViews, appWidgetId: Int, meme: Meme) {
+    private suspend fun updateMemeUI(
+        context: Context,
+        views: RemoteViews,
+        appWidgetId: Int,
+        meme: Meme
+    ) {
         withContext(Dispatchers.Main) {
-
             val awt : AppWidgetTarget = object : AppWidgetTarget(context, R.id.img_widget_meme, views, appWidgetId) {
                 override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
                     super.onResourceReady(resource, transition)
@@ -98,11 +87,19 @@ class MemeWidget : AppWidgetProvider() {
     }
 
     override fun onReceive(context: Context, intent: Intent?) {
-        super.onReceive(context, intent)
+
 
         if (ON_CLICK_MEME == intent?.action) {
+            Log.d("CLICK", "CLICK")
+            val appWidgetId = AppWidgetManager.getInstance(context).getAppWidgetIds(
+                ComponentName(context.packageName, this::class.java.name)
+            )
             val views = RemoteViews(context.packageName, R.layout.meme_widget)
-            requestMeme(context, views, intent.getIntExtra("APP_WIDGET_ID", 0))
+
+            requestMeme(context, views, appWidgetId[0])
+        }
+        else {
+            super.onReceive(context, intent)
         }
     }
 
